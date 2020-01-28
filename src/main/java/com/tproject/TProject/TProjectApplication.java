93a7a3f32b089d9f750c5db645197dfe7560c9ee
venustuspl <venustus.pl@gmail.com>
2020-01-28 22:47:59 +0100

    0.1.1

---
+++ src/main/java/com/tproject/TProject/TProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tproject.TProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tproject/TProject/TProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tproject.TProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
